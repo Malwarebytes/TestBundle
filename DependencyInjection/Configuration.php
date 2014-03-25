a421487e331d3e247ee5f580138cc578b2d9f451
Jonathan Chan <jchan@malwarebytes.org>
2014-03-25 12:00:51 -0400

    Created configuration settings

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,11 +18,21 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('malwarebytes_test');
+        $rootNode = $treeBuilder->root('testbundle');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
+
+        $rootNode
+            ->children()
+                ->scalarNode('doctrine_migration_test_driver')
+                    ->defaultValue('DropReload')
+                    ->info('Configures which driver')
+                    ->example('example setting')
+                ->end()
+            ->end()
+        ;
 
         return $treeBuilder;
     }
