e618acceb3c3e152d90d2286ba90c4fa980fc8a9
Jonathan Chan <jchan@malwarebytes.org>
2014-03-25 12:11:54 -0400

    Implemented Driver support for MigrationTestCase

--- Test/DoctrineMigrationTestCase.php
+++ Test/DoctrineMigrationTestCase.php
@@ -11,6 +11,7 @@ namespace Malwarebytes\TestBundle\Test;
 
 use Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand;
 use Doctrine\ORM\EntityManager;
+use Malwarebytes\TestBundle\Drivers\TestCaseDriver;
 use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Bundle\FrameworkBundle\Console\Application as App;
@@ -33,28 +34,35 @@ class DoctrineMigrationTestCase extends BaseWebTestCase {
     protected $application;
     /** @var  EntityManager */
     protected $em;
+    /** @var  TestCaseDriver */
+    protected $driver;
+
+
 
     public function setUp()
     {
         parent::setUp();
 
-        $this->em = self::$kernel->getContainer()
-            ->get('doctrine')
-            ->getManager();
+        $config=$this->client->getContainer()->getParameter('malwarebytes_test.config');
+        $driver = '\\Malwarebytes\\TestBundle\\Drivers\\MigrationTestCase\\'.$config['doctrine_migration_test_driver'];
+        if (!class_exists($driver)) {
+            throw new \Exception("DoctrineMigrationTestCase Driver '$driver' does not exist. There is a configuration problem in testbundle:doctrine_migration_test_driver.");
+        }
+        $this->driver = new $driver();
+        if (!is_subclass_of($this->driver,"\\Malwarebytes\\TestBundle\\Drivers\\TestCaseDriver")) {
+            throw new \Exception("$driver does not properly implement '\\Malwarebytes\\TestBundle\\Drivers\\TestCaseDriver'. Please correct this.");
+        }
 
-        $tool = new \Doctrine\ORM\Tools\SchemaTool($this->em);
-        $tool->dropDatabase();
-
-        $this->application = new App(self::$kernel);
-        $this->application->add(new MigrateCommand());
-        $this->application->setAutoExit(false);
-
-        $input = new ArrayInput(array('command' => 'doctrine:migrations:migrate', '-q' => true, '-n' => true));
-        $this->application->run($input, null);
+        $this->driver->setUp($this->client);
     }
 
     public function __construct(Client $client = null)
     {
         $this->client = $client;
+    }
+
+    public function tearDown()
+    {
+        $this->driver->tearDown($this->client);
     }
 }
